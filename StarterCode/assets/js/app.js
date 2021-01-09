2793664a66219f957c8fb70e6b0fb77bc738c438
micale24 <kaihall.usaf@gmail.com>
2021-01-09 10:01:47 -0500

    Need to figure out scatter

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,16 +1,15 @@
 
 //Read in data csv
-d3.csv("./assets/data/data.csv").then(function(data) {
-    console.log(data);
+// d3.csv("./assets/data/data.csv").then(function(data) {
+//     console.log(data);
 
-    var age = data.map(data => data.age);
-    console.log(age)
+//     var age = data.map(data => data.age);
+//     console.log(age)
 
-    var obesity = data.map(data => data.obesity);
-    console.log(obesity);
+//     var obesity = data.map(data => data.obesity);
+//     console.log(obesity);
+// }
 
-    //#scatter
-    <script>
 
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
@@ -18,7 +17,7 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#scatter")
+var svg = d3.selectAll("#scatter")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -26,49 +25,33 @@ var svg = d3.select("#scatter")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
- //Read the data
- d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv", function(data) {
+// //Read the data
+// d3.csv("./assets/data/data.csv").then(function(data) {
 
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([0, 4000])
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+  // // Add X axis
+  // var x = d3.scaleLinear()
+  //   .domain([0, 4000])
+  //   .range([ 0, width ]);
+  // svg.append("g")
+  //   .attr("transform", "translate(0," + height + ")")
+  //   .call(d3.axisBottom(x));
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 500000])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+  // // Add Y axis
+  // var y = d3.scaleLinear()
+  //   .domain([0, 500000])
+  //   .range([ height, 0]);
+  // svg.append("g")
+  //   .call(d3.axisLeft(y));
 
-  // Add dots
-  svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(d.GrLivArea); } )
-      .attr("cy", function (d) { return y(d.SalePrice); } )
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
+  // // Add dots
+  // svg.append('g')
+  //   .selectAll("dot")
+  //   .data(data)
+  //   .enter()
+  //   .append("circle")
+  //     .attr("cx", function (d) { return x(d.GrLivArea); } )
+  //     .attr("cy", function (d) { return y(d.SalePrice); } )
+  //     .attr("r", 1.5)
+  //     .style("fill", "#69b3a2")
 
-})
-
-</script>
-
-//   // Cast each hours value in tvData as a number using the unary + operator
-//   tvData.forEach(function(data) {
-//     data.hours = +data.hours;
-//     console.log("Name:", data.name);
-//     console.log("Hours:", data.hours);
-//     });
-//     }).catch(function(error) {
-//     console.log(error);
-    
-
-});
-
-//Choosing variables to compare
+//})
